3d50531b8d2acb70b8fd2cfbe94614b8c90923e8
fabr404 <fabriciofelixsilva11@gmail.com>
2024-09-25 14:15:07 -0300

    atv25

---
+++ atv_25.py
@@ -0,0 +1,17 @@
+# Enunciado: Crie um programa que receba a nota de 5 alunos e exiba quantos foram aprovados (nota maior ou igual a 7).
+
+# variavel
+
+aprovados = 0
+
+# agora sim
+
+for _ in range(5):
+    if float(input("Digite a nota do aluno: ")) >= 7:
+        aprovados += 1
+
+# exibir isso ai
+
+print(f"Quantidade de alunos aprovados foi {aprovados} o resto nao se exforça e copia do GitHub dos outos sjssksk")
+
+# cabo
